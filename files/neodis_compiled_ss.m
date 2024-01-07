2efe650158fe437ff7923a314059e594ef99298f
smydtt <smydtt@gmail.com>
2024-01-07 11:20:56 +0530

    add my info to config file

---
+++ files/neodis_compiled_ss.m
@@ -0,0 +1,8 @@
+clear; clc; tic;
+syms A alpha delta n sigma rho phi theta k c lambda mu ; 
+alpha = 0.5;
+eqn1 = (((1 - lambda*(1 - alpha))/(k^(1 - alpha))) - (rho + delta + n)) ;
+eqn2 = phi*(1 - lambda) - A*theta*(1 - alpha)*lambda*(k^ alpha) ;
+SS1 = solve([eqn1 ==0, eqn2 ==0],[k,lambda],'returnconditions',true,'IgnoreAnalyticConstraints',true)
+E1 = [SS1.k(1); SS1.lambda(1)]
+E2 = [SS1.k(2); SS1.lambda(2)]
