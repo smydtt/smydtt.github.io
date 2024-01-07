723b4f6d2d15892a7b612cb861db51852e83f6d6
smydtt <smydtt@gmail.com>
2024-01-07 11:38:41 +0530

    add my info to config file

--- files/neodis_compiled_ss.m
+++ files/neodis_compiled_ss.m
@@ -6,3 +6,4 @@ eqn2 = phi*(1 - lambda) - A*theta*(1 - alpha)*lambda*(k^ alpha) ;
 SS1 = solve([eqn1 ==0, eqn2 ==0],[k,lambda],'returnconditions',true,'IgnoreAnalyticConstraints',true)
 E1 = [SS1.k(1); SS1.lambda(1)]
 E2 = [SS1.k(2); SS1.lambda(2)]
+toc
